499612db0c97a0b5c9a09cc60e0fd2826409675d
Arcady Oboukhov <arcady.oboukhov@gmail.com>
2025-05-07 16:19:18 -0400

    Update manager.cpp

--- src/manager.cpp
+++ src/manager.cpp
@@ -10,7 +10,6 @@
 #include <QString>
 #include <QLineEdit>
 
-// Constructor for the Form class
 manager::manager(password *pass, QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::manager)
@@ -28,13 +27,12 @@ manager::manager(password *pass, QWidget *parent)
 void manager::saveData() {
     QJsonArray jsonArray;
 
-    // Loop through the indices to gather data from 1 through 18
     for (int i = 0; i < 18; ++i) {
         QString nameEditName = QString("nameTextEdit_%1").arg(i + 1);
         QString usernameEditName = QString("usernameTextEdit_%1").arg(i + 1);
         QString passwordEditName = QString("passwordTextEdit_%1").arg(i + 1);
 
-        // Find the QLineEdit pointers
+        //  QLineEdit pointers
         QLineEdit *nameEdit = this->findChild<QLineEdit*>(nameEditName);
         QLineEdit *usernameEdit = this->findChild<QLineEdit*>(usernameEditName);
         QLineEdit *passwordEdit = this->findChild<QLineEdit*>(passwordEditName);
@@ -51,34 +49,33 @@ void manager::saveData() {
     }
 
     QJsonDocument jsonDoc(jsonArray);
-    QByteArray jsonData = jsonDoc.toJson(QJsonDocument::Compact); // Compact format
+    QByteArray jsonData = jsonDoc.toJson(QJsonDocument::Compact); 
 
-    // Remove the unnecessary formatting
     QString formattedJson = QString::fromUtf8(jsonData);
 
     qDebug() << "Formatted JSON data:" << formattedJson;
 
-    // Pass the JSON string to the password class for saving
-    pass->save(formattedJson); // Assuming you have a save function to handle storing this
+
+    pass->save(formattedJson); 
 }
 
 
 
 
 void manager::loadLastSave() {
-    // Call the method in the password class to get the last saved data
+    
     QByteArray jsonData = pass->get();
 
-    // Parse the JSON data
+    
     QJsonDocument jsonDoc = QJsonDocument::fromJson(jsonData);
     if (jsonDoc.isNull() || !jsonDoc.isArray()) {
         qDebug() << "Failed to parse JSON data or not an array.";
-        return; // Optionally show an error message to the user here
+        return; 
     }
 
     QJsonArray jsonArray = jsonDoc.array();
 
-    // Ensure not overriding more UI elements than available
+    
     for (int i = 0; i < jsonArray.size() && i < 18; ++i) {
         QJsonObject jsonObj = jsonArray[i].toObject();
 
@@ -87,12 +84,12 @@ void manager::loadLastSave() {
         QString username = jsonObj["username"].toString();
         QString password = jsonObj["password"].toString();
 
-        // Set the names of the QLineEdit objects properly
+        // Set the names of the QLineEdit objects
         QString nameEditName = QString("nameTextEdit_%1").arg(i + 1);
         QString usernameEditName = QString("usernameTextEdit_%1").arg(i + 1);
         QString passwordEditName = QString("passwordTextEdit_%1").arg(i + 1);
 
-        // Find the QLineEdit pointers
+        //  QLineEdit pointers
         QLineEdit *nameEdit = this->findChild<QLineEdit*>(nameEditName);
         QLineEdit *usernameEdit = this->findChild<QLineEdit*>(usernameEditName);
         QLineEdit *passwordEdit = this->findChild<QLineEdit*>(passwordEditName);
